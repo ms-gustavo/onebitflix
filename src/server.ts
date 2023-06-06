33311c9fa979f0ef4a017e122bc1a1e725eaa81e
Gustavo Maia <ms.gustavo@outlook.com>
2023-06-06 16:20:06 -0300

    configurando adminjs

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { sequelize } from "./database";
+import { adminJS, adminJsRouter } from "./adminjs";
 
 const app = express();
+app.use(express.static("public"));
+//app.use(caminho, rotas)
+app.use(adminJS.options.rootPath, adminJsRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
